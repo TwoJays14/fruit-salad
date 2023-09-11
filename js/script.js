ca83bc6fb0f807658ae801434d65e874a94212ba
JJatau <jjatau14@outlook.com>
2023-09-11 12:06:39 +0100

    ready to use apis

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const fruitForm = document.querySelector('#inputSection form');
+const fruitList = document.querySelector('#fruitSection ul');
+
+
+const addFruit = fruit => {
+  const li = document.createElement('li');
+  li.textContent = fruit 
+  fruitList.appendChild(li);
+
+  li.addEventListener('click', () => {
+    fruitList.removeChild(li)
+  })
+}
+
+fruitForm.addEventListener('submit', (e) => {
+  e.preventDefault(); // prevent
+  addFruit(e.target.fruitInput.value);
+  e.target.fruitInput.value = ""
+});
+
+
+
+
+
+
